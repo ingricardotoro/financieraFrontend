1913e840d47181cce4fda377b3d4a884975e72af
Marvin Ricardo Toro <ingricardotoro@gmail.com>
2020-11-05 12:14:09 -0600

    Ruta del servidor API

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
-//export const URL_API = 'https://backend-credisas.herokuapp.com/api'
-export const URL_API = 'http://localhost:4000/api'
+export const URL_API = 'https://backend-credisas.herokuapp.com/api'
+    //export const URL_API = 'http://localhost:4000/api'
