8d3e4b6d4ede25ab4d8688bd697233559c74c5c6
Marvin Ricardo Toro <ingricardotoro@gmail.com>
2020-11-12 23:53:25 -0600

    Nuevas Librerias

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,28 +1,5 @@
 /*var width = $(window).width(), height = $(window).height();
 alert('width : ' +width + 'height : ' + height);*/
-"use strict";
-// Loading Array Data
-var data = [{
-    id: 0,
-    text: 'enhancement'
-}, {
-    id: 1,
-    text: 'bug'
-}, {
-    id: 2,
-    text: 'duplicate'
-}, {
-    id: 3,
-    text: 'invalid'
-}, {
-    id: 4,
-    text: 'wontfix'
-}];
-
-$("#js-example-data-array").select({
-    data: data
-});
-
 
 $(document).ready(function() {
     var $window = $(window);
