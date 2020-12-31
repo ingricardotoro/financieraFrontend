0c3d1bd000d872ce7a3404a9947154e6379042f9
Marvin Ricardo Toro <ingricardotoro@gmail.com>
2020-12-30 21:53:53 -0600

    Add Stepper in new Request

--- src/page/Requests/NewRequestPage.js
+++ src/page/Requests/NewRequestPage.js
@@ -1,225 +1,119 @@
 import React, { useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Stepper from '@material-ui/core/Stepper';
-import Step from '@material-ui/core/Step';
-import StepLabel from '@material-ui/core/StepLabel';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-
-
 import { Link } from 'react-router-dom';
-import FormCustomerNewRequest from '../../components/requests/FormCustomerNewRequest';
-import FormLoanNewRequest from '../../components/requests/FormLoanNewRequest';
-import FormReferencesNewRequest from '../../components/requests/FormReferencesNewRequest';
-import FormWarrantyNewRequest from '../../components/requests/FormWarrantyNewRequest';
+import Stepper from 'bs-stepper'
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-  },
-  button: {
-    marginRight: theme.spacing(1),
-  },
-  instructions: {
-    marginTop: theme.spacing(1),
-    marginBottom: theme.spacing(1),
-  },
-
-  stepperCustomer: {
-    display: 'block',
-    [theme.breakpoints.up('sm')]: {
-      display: 'flex',
-    },
-  },
-
-}));
-
-function getSteps() {
-  return ['Cliente', 'Préstamos', 'Referencias', 'Garantias'];
-}
-
-function getStepContent(step) {
-
-  switch (step) {
-    case 0:
-      return <FormCustomerNewRequest />;
-    case 1:
-      return <FormLoanNewRequest />
-    case 2:
-      return <FormReferencesNewRequest />
-    case 3:
-        return <FormWarrantyNewRequest />
-    default:
-      return 'Etapa Desconocida';
-  }
-}
+let stepper = new Stepper(document.querySelector('.bs-stepper'))
 
 
 export default function NewRequestPage() {
-  const classes = useStyles();
-  const [activeStep, setActiveStep] = React.useState(0);
-  const [skipped, setSkipped] = React.useState(new Set());
-  const steps = getSteps();
 
-  const [newRequest, setNewRequest] = useState({})
-
-  const isStepOptional = (step) => {
-    //return step === 2;
-  };
-
-  const isStepSkipped = (step) => {
-    return skipped.has(step);
-  };
-
-  const handleNext = () => {
-
-    let newSkipped = skipped;
-    if (isStepSkipped(activeStep)) {
-      newSkipped = new Set(newSkipped.values());
-      newSkipped.delete(activeStep);
+    const next=()=>{
+        stepper.next()
     }
 
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setSkipped(newSkipped);
-  };
-
-  const handleBack = () => {
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  };
-
-  const handleSkip = () => {
-    if (!isStepOptional(activeStep)) {
-      // You probably want to guard against something like this,
-      // it should never occur unless someone's actively trying to break something.
-      throw new Error("You can't skip a step that isn't optional.");
+    const previous = () =>{
+        stepper.previous()
     }
-
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    setSkipped((prevSkipped) => {
-      const newSkipped = new Set(prevSkipped.values());
-      newSkipped.add(activeStep);
-      return newSkipped;
-    });
-  };
-
-  const handleReset = () => {
-    setActiveStep(0);
-  };
-
-  const handleFinal = () =>{
-
-  }
-
-  return (
-<div className="pcoded-content">
-  <div className="pcoded-inner-content">
-    <div className="main-body">
-        <div className="page-wrapper">
-        {/* Page-header start */}
-        <div className="page-header mt-5">
-            <div className="page-header-title">
-                <h4>Creación de Nuevas Solicitudes</h4>
-                <span>*Es necesario llenar todos los datos que sean obligarorios.</span>
-            </div>
-            <div className="page-header-breadcrumb">
-                <ul className="breadcrumb-title">
-                    <li className="breadcrumb-item">
-                    <Link to="/solicitudes">
-                        <i className="icofont icofont-edit" />
-                    </Link>
-                    </li>
-                    <li className="breadcrumb-item">
-                    <Link to="/solicitudes">
-                            Ver Solicitudes
-                    </Link>
-                    </li>
-                </ul>
-            </div>
-        </div>
   
-    <div className="page-body">
-    <div className="row">
-        <div className="col-sm-12">
-        {/* Form wizard with validation card start */}
-        <div className="card">
-            <div className="card-block">
-                 <div className={classes.root} >
-                    <Stepper activeStep={activeStep} className={classes.stepperCustomer} >
-                        {steps.map((label, index) => {
-                        const stepProps = {};
-                        const labelProps = {};
-                        if (isStepOptional(index)) {
-                            labelProps.optional = <Typography variant="caption">Opcional</Typography>;
-                        }
-                        if (isStepSkipped(index)) {
-                            stepProps.completed = false;
-                        }
-                        return (
-                            <Step key={label} {...stepProps}>
-                            <StepLabel {...labelProps}>{label}</StepLabel>
-                            </Step>
-                        );
-                        })}
-                    </Stepper>
-                    <div>
-                        {activeStep === steps.length ? (
-                        <div className="row" style={{paddingTop:"10px"}}>
-
-                            <div className="col-md-4 col-sm-12">
-                              <button onClick={handleBack} className="btn btn-primary form-control">
-                                  VOLVER
-                              </button>
+  return (
+    
+    <div className="pcoded-content">
+        <div className="pcoded-inner-content">
+        <div className="main-body">
+            <div className="page-wrapper">
+            {/* Page-header start */}
+            <div className="page-header mt-5">
+                <div className="page-header-title">
+                    <h4>Creación de Nuevas Solicitudes</h4>
+                    <span>*Es necesario llenar todos los datos que sean obligarorios.</span>
+                </div>
+                <div className="page-header-breadcrumb">
+                    <ul className="breadcrumb-title">
+                        <li className="breadcrumb-item">
+                        <Link to="/solicitudes">
+                            <i className="icofont icofont-edit" />
+                        </Link>
+                        </li>
+                        <li className="breadcrumb-item">
+                        <Link to="/solicitudes">
+                                Ver Solicitudes
+                        </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+    
+        <div className="page-body">
+        <div className="row">
+            <div className="col-sm-12">
+                <div className="card">
+                    <div className="card-block">
+                    <div className="bs-stepper">
+                        
+                        <div className="bs-stepper-header" role="tablist">
+                            {/* your steps here */}
+                            <div className="step" data-target="#logins-part">
+                            <button type="button" className="step-trigger" role="tab" aria-controls="logins-part" id="logins-part-trigger">
+                                <span className="bs-stepper-circle">1</span>
+                                <span className="bs-stepper-label">Logins</span>
+                            </button>
                             </div>
-                            <div className="col-md-4 col-sm-12" style={{textAlign:"center"}}> 
-                              <label htmlFor="">Ha terminado de llenar los datos de la solicitud, puedes volver a revisar los datos  o terminar de registrar la solicitud</label>
+                            <div className="line" />
+                            <div className="step" data-target="#segunda">
+                            <button type="button" className="step-trigger" role="tab" aria-controls="segunda" id="segunda-trigger">
+                                <span className="bs-stepper-circle">2</span>
+                                <span className="bs-stepper-label">Various information</span>
+                            </button>
                             </div>
-                            <div className="col-md-4 col-sm-12" style={{textAlign:"center"}}> 
-                            <button onClick={handleFinal} className="btn btn-success form-control">
-                                  Registrar Solicitud
-                              </button>
+                            <div className="line" />
+                            <div className="step" data-target="#tercera">
+                            <button type="button" className="step-trigger" role="tab" aria-controls="tercera" id="tercera-trigger">
+                                <span className="bs-stepper-circle">3</span>
+                                <span className="bs-stepper-label">Tercera</span>
+                            </button>
                             </div>
-
-                        </div>
-                        ) : (
-                        <div>
-                            <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
-                            <div>
-                            <Button disabled={activeStep === 0} onClick={handleBack} className={classes.button}>
-                                Anterior
-                            </Button>
-                            {isStepOptional(activeStep) && (
-                                <Button
-                                variant="contained"
-                                color="primary"
-                                onClick={handleSkip}
-                                className={classes.button}
-                                >
-                                Saltar
-                                </Button>
-                            )}
-
-                            <Button
-                                variant="contained"
-                                color="primary"
-                                onClick={handleNext}
-                                className={classes.button}
-                            >
-                                {activeStep === steps.length - 1 ? 'Finalizar' : 'Siguiente'}
-                            </Button>
+                            <div className="line" />
+                            <div className="step" data-target="#cuarta">
+                            <button type="button" className="step-trigger" role="tab" aria-controls="cuarta" id="cuarta-trigger">
+                                <span className="bs-stepper-circle">4</span>
+                                <span className="bs-stepper-label">Cuarta Etapa</span>
+                            </button>
                             </div>
                         </div>
-                        )}
+                        <div className="bs-stepper-content">
+                            {/* your steps content here */}
+                            <div id="logins-part" className="content fade" role="tabpanel" aria-labelledby="logins-part-trigger">
+                                <h1>Primera Etapa</h1>
+                                <button className="btn btn-primary btn-sm" onClick={()=>{next()}}>Siguiente</button>
+                            </div>
+                            <div id="segunda" className="content fade" role="tabpanel" aria-labelledby="segunda-trigger" > 
+                                <h1>Segunda Etapa</h1>
+                                <button className="btn btn-primary btn-sm" onClick={()=>{previous()}}>Anterior</button>
+                                <button className="btn btn-primary btn-sm" onClick={()=>{next()}}>Siguiente</button>
+                            </div>
+                            <div id="tercera" className="content fade" role="tabpanel" aria-labelledby="tercera-trigger" > 
+                                <h1>Tercera Etapa</h1>
+                                <button className="btn btn-primary btn-sm" onClick={()=>{previous()}}>Anterior</button>
+                                <button className="btn btn-primary btn-sm" onClick={()=>{next()}}>Siguiente</button>
+                            </div>
+                            <div id="cuarta" className="content fade" role="tabpanel" aria-labelledby="cuarta-trigger" > 
+                                <h1>Cuarta Etapa</h1>
+                                <button className="btn btn-primary btn-sm" onClick={()=>{previous()}}>Anterior</button>
+                                <button className="btn btn-primary btn-sm" onClick={()=>{next()}}>Finalizar</button>
+                            </div>
+                        </div>
+                        </div>
+
                     </div>
                 </div>
-              </div>
             </div>
-          </div>
         </div>
-       </div>
-      </div>
-     </div>
+        </div>
+        </div>
+        </div>
+        </div>
     </div>
-</div>
 
   );
 }
+
